2b552dabaae1f5dd35a47035033db17a33b1ded6
Jayed Mohamed Rashid <jayed.dev@gmail.com>
2022-08-09 11:03:02 +0600

    local modules added

---
+++ local modules/index.js
@@ -0,0 +1,13 @@
+// Accessing Modules
+const profile = require('./others');
+
+console.log(profile.getUser());
+console.log(profile.getProfession());
+console.log(profile.getMovie());
+console.log(profile.ratingIMDB);
+
+// run "node index.js" from terminal, which returns
+// Tom Cruise
+// Actor
+// Mission Impossible
+// 7.1
---
+++ local modules/others.js
@@ -0,0 +1,22 @@
+// Local Module Creation:
+// Functions
+const getUser = () => {
+    return "Tom Cruise";
+}
+
+const getProfession = () => {
+    return "Actor";
+}
+
+const getMovie = () => {
+    return "Mission Impossible";
+}
+
+// Variables
+const ratingIMDB = 7.1;
+
+// Export Functions
+exports.getUser = getUser;
+exports.getProfession = getProfession;
+exports.getMovie = getMovie;
+exports.ratingIMDB = ratingIMDB;
